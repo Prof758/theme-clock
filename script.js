8f06570090e7bde95e1ef21480df906c2af89d65
Simon Prophet <simon.s.prophet@outlook.com>
2024-11-26 22:35:55 +0000

    completed functionality for the clock

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
-const date = document.querySelector('.date');
+const dateEl = document.querySelector('.date');
 
 const days = [
   'Sunday',
@@ -42,3 +42,58 @@ function setToggleButtonText() {
     toggle.innerText = 'Dark mode';
   }
 }
+
+function setTime() {
+  const time = new Date();
+  const day = time.getDay();
+  const date = time.getDate();
+  const month = time.getMonth();
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  // console.log(time, month, hours, minutes);
+
+  // StackOverflow
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.innerHTML = `${hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  // timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`; 12 hour clock.
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} 
+    <span class="circle">${date}</span>
+  `;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
